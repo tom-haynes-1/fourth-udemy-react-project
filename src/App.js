fe1acf2181e6217603fd073df60661ba7e9cae91
Tom Haynes <tomhaynes94@gmail.com>
2023-04-23 13:23:27 +0100

    changed project title and fixed app function declaration

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-function App() {
+import React from "react";
+
+const App = () => {
   return (
     <div>
-      <h2>Let's get started! Hello</h2>
+      <h2>Let's get started!</h2>
     </div>
   );
 }
