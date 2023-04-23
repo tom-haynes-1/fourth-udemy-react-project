b217a09408ce223308dbdf67adbe54e3cdb2bc42
Tom Haynes <tomhaynes94@gmail.com>
2023-04-23 15:21:55 +0100

    refactored map() method to include MealItem component and rendered a styled list of meals from mocked data

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -2,12 +2,18 @@ import React from "react";
 import classes from "./AvailableMeals.module.css";
 import { mockedMealsData }  from "../../mocked-data/mocked-meals-data";
 
+import MealItem from "./MealItem/MealItem";
 import Card from "../UI/Card";
 
 const AvailableMeals = (props) => {
-    const mealsListArray = mockedMealsData.map(mealItem => {
-        return <li>{ mealItem.name }</li>
-    });
+    const mealsListArray = mockedMealsData.map((mealItem) => 
+      <MealItem 
+        key={ mealItem.id } 
+        mealName={ mealItem.name }
+        mealDescription={ mealItem.description }
+        mealPrice={ mealItem.price }
+      />
+    );
 
   return (
     <section className={ classes.meals }>
