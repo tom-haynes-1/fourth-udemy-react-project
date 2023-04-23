887d91f7e89d4674eac2dbcba09bfaf04e36b013
Tom Haynes <tomhaynes94@gmail.com>
2023-04-23 16:03:56 +0100

    Created reusable Input component

---
+++ src/components/UI/Input.js
@@ -0,0 +1,15 @@
+import React from "react";
+import classes from "./Input.module.css";
+
+const Input = (props) => {
+  return (
+    <div className={ classes.input }>
+      <label htmlFor={ props.input.id }>
+        { props.labelText }
+      </label>
+      <input { ...props.input } />
+    </div>
+  );
+};
+
+export default Input;
